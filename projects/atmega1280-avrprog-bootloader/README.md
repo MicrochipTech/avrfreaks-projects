1774917eb550fadb98b54d434b1f21a4e8446844
Ultralisk2005 <Ultralisk2005@avrfreaks.net>
2010-07-20 11:30:00 -0700

    Import from https://community.atmel.com/projects/atmega1280-avrprog-bootloader

---
+++ projects/atmega1280-avrprog-bootloader/README.md
@@ -0,0 +1,16 @@
+# Atmega1280 AVRProg Bootloader
+
+Uploaded by Ultralisk2005 on 2010-07-20 11:30:00 (rating 0 out of 5)
+
+## Summary
+
+Modified from atmega128
+
+## Compilers
+
+- WinAVR (GNU GCC)
+
+## Tags
+
+- Complete code
+- WinAVR (GNU GCC)
---
+++ projects/atmega1280-avrprog-bootloader/atmega1280_bootloader/.dep/main.o.d
@@ -0,0 +1,65 @@
+main.o: main.c c:/winavr/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/iom1280.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/iomxx0_1.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/wdt.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/boot.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/eeprom.h \
+  c:\winavr\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  c:\winavr\bin\../lib/gcc/avr/4.3.3/include-fixed/limits.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/interrupt.h \
+  c:/winavr/lib/gcc/../../avr/include/util/delay.h \
+  c:/winavr/lib/gcc/../../avr/include/util/delay_basic.h chipdef.h \
+  mega1280.h
+
+c:/winavr/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr/lib/gcc/../../avr/include/avr/iom1280.h:
+
+c:/winavr/lib/gcc/../../avr/include/avr/iomxx0_1.h:
+
+c:/winavr/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/winavr/lib/gcc/../../avr/include/avr/wdt.h:
+
+c:/winavr/lib/gcc/../../avr/include/avr/boot.h:
+
+c:/winavr/lib/gcc/../../avr/include/avr/eeprom.h:
+
+c:\winavr\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+c:\winavr\bin\../lib/gcc/avr/4.3.3/include-fixed/limits.h:
+
+c:/winavr/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:/winavr/lib/gcc/../../avr/include/avr/interrupt.h:
+
+c:/winavr/lib/gcc/../../avr/include/util/delay.h:
+
+c:/winavr/lib/gcc/../../avr/include/util/delay_basic.h:
+
+chipdef.h:
+
+mega1280.h:
