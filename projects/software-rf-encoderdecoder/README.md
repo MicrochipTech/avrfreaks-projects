be675dfd7f6e081535a20a36adfb744cdb7b1bcb
newbie123 <newbie123@avrfreaks.net>
2011-04-10 21:10:00 -0700

    Import from https://community.atmel.com/projects/software-rf-encoderdecoder

---
+++ projects/software-rf-encoderdecoder/README.md
@@ -0,0 +1,31 @@
+# Software RF encoder/decoder
+
+Uploaded by newbie123 on 2011-04-10 21:10:00 (rating 0 out of 5)
+
+## Summary
+
+This project is an encoder/decoder for cheap RF modules  
+
+specifically the 434MHz ones. It is written entirely in assembly.  
+
+You can connect the avr directly to the RF modules. This  
+
+system is very reliable.
+
+
+The timing is tight and carefully calibrated to use only a 3.6864MHz clock. This encoder/decoder pair works with 8 bit data, and an 8 bit address. 
+
+
+Please read the "readthis.txt" file for full details.
+
+
+If you have any questions, or want to offer some improvements to the code,please message me here on avrfreaks.
+
+## Compilers
+
+- AVR Assembler
+
+## Tags
+
+- Complete code
+- AVR Assembler
---
+++ projects/software-rf-encoderdecoder/RF/RF/TX/90S1200/TX.asm
@@ -0,0 +1,76 @@
+;Monday, March 15, 2010, 10:43:09 AM
+;uses encoding and Software Bit-Bang routines
+;uses 3.6864 MHz clock
+;
+.NOLIST
+.include "1200def.inc"
+.LIST
+;
+.def	count		= r4
+.def	dcount	= r5
+.def	dcount2	= r6
+.def	dcount3	= r7
+
+.def	temp	 =	r16
+.def	bitcnt = 	r17	;bit counter
+.def	Txbyte = 	r18	;needed for transmit routine
+
+.equ	address 	= 0x0A
+.equ command 	= 0x69
+;
+;PortD
+.equ	TxD	= 1		;TX pin is PD1 (output)
+;--------------------------------------------------------
+.cseg
+.org 0
+	rjmp	Start
+;--------------------------------------------------------
+.org 0x13
+Start:
+;	ldi  temp,low(RAMEND)	;Initialize stackpointer
+;	out	SPL,temp		
+
+	ldi	temp,0b11111111
+	out	PORTD,temp
+	ldi	temp,0b11111010	;PD0,PD2 = inputs
+	out	DDRD,temp
+;--------------------------------------------------------
+Main:
+	sbic	PIND,2		;wait for button to be pushed
+	rjmp	Main
+
+	ldi	temp,50
+	mov	dcount,temp
+	rcall	Delay			;50mS delay for debouncing
+MainA:
+	sbis	PIND,2		;wait for button to be released
+	rjmp	MainA
+;---------------------------------------------------------------
+	ldi	TxByte,0x55
+	rcall	PreambleTX
+
+	ldi	TxByte,0xAA
+	rcall	PreambleTX
+
+	ldi	TxByte,0xFF
+	rcall	PreambleTX
+
+	ldi	temp,4		;send the packet 4 times
+	mov	count,temp
+	rcall	startbit
+
+TXLoop:
+	ldi	TxByte,address
+	rcall	TX_Byte
+	ldi	TxByte,command
+	rcall	TX_Byte
+
+	dec	count
+	brne	TXLoop
+
+	rjmp	main
+;***************************************************************
+.include "softuart.inc"
+.include "delay.inc"
+;***************************************************************
+.exit
