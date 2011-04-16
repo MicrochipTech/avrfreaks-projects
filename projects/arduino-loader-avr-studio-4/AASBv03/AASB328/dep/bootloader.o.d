5c0b32cbb353dbac6ba3b9a38c6bfa413e435a80
o5o5o5 <o5o5o5@avrfreaks.net>
2011-04-16 10:15:00 -0700

    Import from https://community.atmel.com/projects/arduino-loader-avr-studio-4

---
+++ projects/arduino-loader-avr-studio-4/AASBv03/AASB328/dep/bootloader.o.d
@@ -0,0 +1,52 @@
+bootloader.o: ../bootloader.c \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/inttypes.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/stdint.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/io.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/sfr_defs.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/iom328p.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/portpins.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/common.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/version.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/fuse.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/lock.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/interrupt.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/boot.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/eeprom.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/include/stddef.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/include-fixed/limits.h \
+ c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/pgmspace.h \
+ ../command.h
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/inttypes.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/stdint.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/io.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/sfr_defs.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/iom328p.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/portpins.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/common.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/version.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/fuse.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/lock.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/interrupt.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/boot.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/eeprom.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/include/stddef.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/include-fixed/limits.h:
+
+c:\embedded\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.4.3/../../../../avr/include/avr/pgmspace.h:
+
+../command.h:
---
+++ projects/arduino-loader-avr-studio-4/AASBv03/readme.txt
@@ -0,0 +1,27 @@
+Arduino Bootloader for AVR Studio 4
+
+This software is a bootloader compatible with AVR Studio 4 for 
+Arduino UNO/Diecimila/Duemilanove with ATmega168/328. Project 
+is based on well known Peter Fleury STK500 v2 bootloader.
+
+To start bootloader connect PIN12 and GND and press RESET on 
+Arduino. L led will show that bootloader is started. Then 
+connect from AVR Studio, select STK500 or AVR-ISP and enjoy.
+
+To burn bootloader you can use any AVR programmer. 
+Don't forget to set BOOTRST fuse and BOOTSZ to 1024 words.
+
+Author:  Yuri A. Nikiforov <nikiforov2000@mail.ru>
+Tested with Arduino UNO (ATmega328P)
+Date: 2011/04/09
+
+TODO: 
+- bug with hw/sw version reporting (reports 0/0)
+- optimize to take less space (1024 word bootloader section)
+
+CHANGE LOG:
+v0.2:
+- merged ATmega168 and ATmega328 projects into one folder
+
+v0.1: 
+- initial release
